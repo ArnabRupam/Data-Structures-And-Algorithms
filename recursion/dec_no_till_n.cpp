07349baabdb794540a870d9976452d1e8d5b56cd
Arnab Bandyopadhyay <88840740+ClaeroBluster@users.noreply.github.com>
2022-02-06 15:10:03 +0530

    Add files via upload

---
+++ recursion/dec_no_till_n.cpp
@@ -0,0 +1,26 @@
+//Print numbers till n in decreasing order
+
+#include<iostream>
+using namespace std;
+
+void dec(int n)
+{
+    if(n==1)
+    {
+        
+        return;
+    }
+
+    cout<<n<<endl;
+    dec(n-1);
+}
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    dec(n);
+    return 0;
+}
