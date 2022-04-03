7e845a920b82cea9a2d86b6fa09cdf0cefbc56b8
Arnab Bandyopadhyay <88840740+ClaeroBluster@users.noreply.github.com>
2022-04-03 09:54:41 +0530

    Create count_frq_ele.cpp

---
+++ hashing/count_frq_ele.cpp
@@ -0,0 +1,26 @@
+#include<map>
+#include<unordered_map>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    map<int,int>m;
+    for(int i=0;i<n;i++)
+    {
+
+        m[a[i]]++;
+    }
+    map<int,int>:: iterator it;
+    for(it=m.begin();it!=m.end();it++)
+    {
+        cout<<it->first<<" "<<it->second<<endl;
+    }
+}
